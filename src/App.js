dbfc053e22617a85bed5794c3e93c86aea24df16
bernard rayoso <rayosobernard@gmail.com>
2023-01-02 16:47:12 +0800

    add navigation feature

--- src/App.js
+++ src/App.js
@@ -1,41 +1,17 @@
 import React from 'react';
-import './category.styles.scss';
-import CategoryItem from './components/category-item/category-item.component';
+import Home from './components/routes/home/home.component';
+import { Route, Routes } from 'react-router-dom';
+import Navigation from './components/navigation/navigation.component';
+
+
 
 const App = () => {
-	const categories = [
-		{
-			id: 1,
-			title: 'hats',
-			imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-		},
-		{
-			id: 2,
-			title: 'jackets',
-			imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
-		},
-		{
-			id: 3,
-			title: 'sneakers',
-			imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
-		},
-		{
-			id: 4,
-			title: 'womens',
-			imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
-		},
-		{
-			id: 5,
-			title: 'mens',
-			imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
-		},
-	];
-	return (
-		<div className='categories-container'>
-			{categories.map((category) => (
-				<CategoryItem key={category.id} category={category} />
-			))}
-		</div>
+		return (
+			<Routes>
+				<Route path='/' element={<Navigation/>} >
+					<Route index element={<Home/>} />
+				</Route>
+			</Routes>
 	);
 };
 
