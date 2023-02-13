5dcc5ae7b78e964f4125776533745a989fcc8f6e
bernard rayoso <rayosobernard@gmail.com>
2023-02-14 00:36:17 +0800

    rearrange files

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
-import React from 'react';
-import Home from './components/routes/home/home.component';
-import { Route, Routes } from 'react-router-dom';
-import Navigation from './components/navigation/navigation.component';
-import SignIn from './components/sign-in/sign-in.component';
-import SignUpForm from './components/sign-up-form/signup-form.component';
-
-
+import React from "react";
+import Home from "./components/routes/home/home.component";
+import { Route, Routes } from "react-router-dom";
+import Navigation from "./components/navigation/navigation.component";
+import SignIn from "./components/routes/sign-in/sign-in.component";
+import SignUpForm from "./components/sign-up-form/signup-form.component";
 
 const App = () => {
-		return (
-			<Routes>
-				<Route path='/' element={<Navigation/>} >
-					<Route index element={<Home/>} />
-					<Route path='sign-in'  element={<SignIn/>} />
-					
-				</Route>
-			</Routes>
-	);
+  return (
+    <Routes>
+      <Route path='/' element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path='sign-in' element={<SignIn />} />
+      </Route>
+    </Routes>
+  );
 };
 
 export default App;
