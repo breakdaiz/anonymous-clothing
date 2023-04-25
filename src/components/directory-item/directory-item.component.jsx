c1637c82493174fd12fb2bd58ffba6d8dd7747d6
bernard rayoso <rayosobernard@gmail.com>
2023-04-26 03:11:51 +0800

    add redirects fix for netlify

--- src/components/directory-item/directory-item.component.jsx
+++ src/components/directory-item/directory-item.component.jsx
@@ -4,11 +4,18 @@ import {
   DirectoryItemContainer,
 } from "./directory-item.styles";
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const DirectoryItem = ({ category }) => {
-  const { imageUrl, title } = category;
+  const { imageUrl, title, route } = category;
+  const navigate = useNavigate();
+
+  const onNavigateHandler = () => {
+    navigate(route);
+  };
+
   return (
-    <DirectoryItemContainer>
+    <DirectoryItemContainer onClick={onNavigateHandler}>
       <BackgroundImage imageUrl={imageUrl} />
       <Body>
         <h2> {title} </h2>
--- src/components/routes/home/home.component.jsx
+++ src/components/routes/home/home.component.jsx
@@ -2,36 +2,7 @@ import React from "react";
 import Directory from "../../directory/directory-component";
 
 const Home = () => {
-  const categories = [
-    {
-      id: 1,
-      title: "HATS",
-      imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
-    },
-    {
-      id: 2,
-      title: "JACKETS",
-      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
-    },
-    {
-      id: 3,
-      title: "SNEAKERS",
-      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
-    },
-    {
-      id: 4,
-      title: "WOMENS",
-      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
-      size: "large",
-    },
-    {
-      id: 5,
-      title: "MENS",
-      imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
-      size: "large",
-    },
-  ];
-  return <Directory categories={categories} />;
+  return <Directory />;
 };
 
 export default Home;
