0202d6b60001cdddcf2f61f9759b45a2be19a773
bernard rayoso <rayosobernard@gmail.com>
2023-05-17 02:25:03 +0800

    implement redux-persist for local storage on web browser

--- src/contexts/checkout.context.jsx
+++
@@ -1,7 +0,0 @@
-import { createContext } from "react";
-
-export const CheckoutContex = createContext({});
-
-export const CheckoutProvider = ({ children }) => {
-  return <CheckoutContex.Provider>{}</CheckoutContex.Provider>;
-};
